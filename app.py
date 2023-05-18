9a61bae95a0f2df289bdb2df8b71dcaec6d6341f
Sanjay Kumar <sanjaykmruk>
2023-05-18 09:13:49 +0000

    updated -1

--- app.py
+++ app.py
@@ -83,25 +83,3 @@ if __name__ == '__main__':
 
 
 
-# import os
-#
-# from flask import (Flask, redirect, render_template, request,
-#                    send_from_directory, url_for)
-#
-# app = Flask(__name__)
-#
-#
-# @app.route('/')
-# def index():
-#    return "please hit the endpoint /hello"
-#
-#
-# @app.route('/hello', methods=['GET'])
-# def hello():
-#    # name = request.form.get('name')
-#     return "hello Sanjay"
-#
-#
-#
-# if __name__ == '__main__':
-#    app.run()
